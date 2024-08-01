561d1c6047808363f4340bc89b541222f2bd6395
TheOrbMaverick <enangita@gmail.com>
2024-08-01 21:34:02 +0100

    personal data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,9 @@ import logging
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str
+        ) -> str:
     """
     Obfuscates specified fields in a log message.
 
@@ -15,13 +17,18 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
         fields: A list of strings representing all fields to obfuscate.
         redaction: A string representing by what the field will be obfuscated.
         message: A string representing the log line.
-        separator: A string representing by which character is separating all fields in the log line.
+        separator: A string representing by which character is separating all
+        fields in the log line.
 
     Returns:
         The obfuscated log message.
     """
     pattern = f'({"|".join(fields)})=.*?{separator}'
-    return re.sub(pattern, lambda m: m.group(0).split('=')[0] + f'={redaction}{separator}', message)
+    return re.sub(
+        pattern,
+        lambda m: m.group(0).split('=')[0] + f'={redaction}{separator}',
+        message
+        )
 
 
 class RedactingFormatter(logging.Formatter):
@@ -36,5 +43,7 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, record.msg, self.SEPARATOR
+            )
         return super().format(record)
