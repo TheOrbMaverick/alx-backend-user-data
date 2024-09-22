a7496d27fce548065d9dbaf55f880190b7b285c2
TheOrbMaverick <enangita@gmail.com>
2024-09-22 16:15:44 +0100

    backend user data

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,17 +7,18 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     """
-    Hashes a password with a salt using bcrypt and returns the salted, hashed password.
-    
+    Hashes a password with a salt using bcrypt and returns the salted,
+    hashed password.
+
     Args:
         password (str): The password to hash.
-    
+
     Returns:
         bytes: The salted, hashed password as a byte string.
     """
     # Generate a salt
     salt = bcrypt.gensalt()
-    
+
     # Return the hashed password with the salt
     return bcrypt.hashpw(password.encode('utf-8'), salt)
 
@@ -25,11 +26,11 @@ def hash_password(password: str) -> bytes:
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """
     Validates that the provided password matches the hashed password.
-    
+
     Args:
         hashed_password (bytes): The hashed password.
         password (str): The plain text password to verify.
-    
+
     Returns:
         bool: True if the password matches, False otherwise.
     """
