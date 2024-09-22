a279a0955efad6eaf3fa8a44000b7432ee289c5f
TheOrbMaverick <enangita@gmail.com>
2024-09-22 19:53:35 +0100

    back end user data handling

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,7 +14,7 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 # Create a variable auth initialized to None
 auth = None
 
-# Based on the environment variable AUTH_TYPE, load and assign the right instance of authentication to auth
+# Based on the environment variable AUTH_TYPE
 auth_type = getenv("AUTH_TYPE", None)
 if auth_type == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
@@ -23,12 +23,14 @@ else:
     from api.v1.auth.auth import Auth
     auth = Auth()
 
+
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """
     Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
+
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
@@ -37,6 +39,7 @@ def forbidden(error) -> str:
     """
     return jsonify({"error": "Forbidden"}), 403
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """
@@ -44,13 +47,16 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.before_request
 def before_request():
     """ Method to handle before_request filter """
     if auth is None:
         return
 
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    excluded_paths = [
+        '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'
+        ]
     if not auth.require_auth(request.path, excluded_paths):
         return
 
@@ -59,6 +65,7 @@ def before_request():
 
     if auth.current_user(request) is None:
         abort(403)
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
