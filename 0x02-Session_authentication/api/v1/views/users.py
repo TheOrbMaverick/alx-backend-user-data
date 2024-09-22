82485c2b25a2c5cf0f9cd5c5f130b1fa5fde48e4
TheOrbMaverick <enangita@gmail.com>
2024-09-22 21:46:00 +0100

    checker 02

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -5,7 +5,7 @@ from api.v1.views import app_views
 from models.user import User
 
 
-@app_views.route('/api/v1/users', methods=['GET'], strict_slashes=False)
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
 def get_users():
     """ GET /api/v1/users """
     users = User.all()
@@ -13,7 +13,7 @@ def get_users():
     return jsonify(users_list)
 
 
-@app_views.route('/api/v1/users/<user_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def get_user(user_id):
     """ GET /api/v1/users/<user_id> """
     if user_id == "me":
@@ -26,21 +26,21 @@ def get_user(user_id):
     return jsonify(user.to_dict())
 
 
-@app_views.route('/api/v1/users', methods=['POST'], strict_slashes=False)
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
 def create_user():
     """ POST /api/v1/users """
     # Implementation here
     pass
 
 
-@app_views.route('/api/v1/users/<user_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
 def update_user(user_id):
     """ PUT /api/v1/users/<user_id> """
     # Implementation here
     pass
 
 
-@app_views.route('/api/v1/users/<user_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id):
     """ DELETE /api/v1/users/<user_id> """
     # Implementation here
