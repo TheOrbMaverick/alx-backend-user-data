c19f17f59eaf6f18999e895363c0447ce3dd217f
TheOrbMaverick <enangita@gmail.com>
2024-09-22 19:48:07 +0100

    checking backend

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -23,13 +23,13 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
-@app_views.route('/api/v1/unauthorized', methods=['GET'], strict_slashes=False)
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """ Endpoint to trigger a 401 error
     """
     abort(401)
 
-@app_views.route('/api/v1/forbidden', methods=['GET'], strict_slashes=False)
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden() -> str:
     """ Endpoint to trigger a 403 error
     """
