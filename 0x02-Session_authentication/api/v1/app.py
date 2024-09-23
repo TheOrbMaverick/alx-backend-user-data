401f24e8dd83db34c27600ed792c0f3c428bd7e6
TheOrbMaverick <enangita@gmail.com>
2024-09-23 15:16:23 +0100

    checker authorization and forbidden

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -32,23 +32,23 @@ else:
     auth = Auth()
 
 
-@app.before_request
-def before_request() -> str:
-    """ Method to filter each request """
-    if auth is None:
-        pass
-    else:
-        setattr(request, "current_user", auth.current_user(request))
-        excluded_paths = ['/api/v1/status/',
-                          '/api/v1/unauthorized/',
-                          '/api/v1/forbidden/',
-                          '/api/v1/auth_session/login/']
-        if auth.require_auth(request.path, excluded_paths):
-            sk = auth.session_cookie(request)
-            if auth.authorization_header(request) is None and sk is None:
-                abort(401, description="Unauthorized")
-            if auth.current_user(request) is None:
-                abort(403, description="Forbidden")
+# @app.before_request
+# def before_request() -> str:
+#     """ Method to filter each request """
+#     if auth is None:
+#         pass
+#     else:
+#         setattr(request, "current_user", auth.current_user(request))
+#         excluded_paths = ['/api/v1/status/',
+#                           '/api/v1/unauthorized/',
+#                           '/api/v1/forbidden/',
+#                           '/api/v1/auth_session/login/']
+#         if auth.require_auth(request.path, excluded_paths):
+#             sk = auth.session_cookie(request)
+#             if auth.authorization_header(request) is None and sk is None:
+#                 abort(401, description="Unauthorized")
+#             if auth.current_user(request) is None:
+#                 abort(403, description="Forbidden")
 
 
 @app.errorhandler(401)
