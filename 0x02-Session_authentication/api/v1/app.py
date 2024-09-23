42b873006dccf7051677843a1810e1db5454a453
TheOrbMaverick <enangita@gmail.com>
2024-09-23 16:09:44 +0100

    checker authorization and forbidden

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -56,23 +56,23 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
-@app.before_request
-def before_request():
-    """ Method to handle before_request filter """
-    if auth is None:
-        return
+# @app.before_request
+# def before_request():
+#     """ Method to handle before_request filter """
+#     if auth is None:
+#         return
 
-    excluded_paths = [
-        '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'
-        ]
-    if not auth.require_auth(request.path, excluded_paths):
-        return
+#     excluded_paths = [
+#         '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'
+#         ]
+#     if not auth.require_auth(request.path, excluded_paths):
+#         return
 
-    if auth.authorization_header(request) is None:
-        abort(401)
+#     if auth.authorization_header(request) is None:
+#         abort(401)
 
-    if auth.current_user(request) is None:
-        abort(403)
+#     if auth.current_user(request) is None:
+#         abort(403)
 
 
 @app.route('/api/v1/status/', methods=['GET'])
